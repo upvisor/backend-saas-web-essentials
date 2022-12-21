0fa4240b758cd874e9cbbbc56626741c276b5785
Jorge Tapia <jorge504tapia@gmail.com>
2022-12-21 12:36:03 -0300

    node mailer

--- src/controllers/suscripcion.controllers.js
+++ src/controllers/suscripcion.controllers.js
@@ -1,5 +1,6 @@
 import Suscripcion from '../models/Suscripcion.js'
 import bizSdk from 'facebook-nodejs-business-sdk'
+import nodemailer from 'nodemailer'
 
 export const createSuscripcion = async (req, res) => {
     try {
@@ -37,6 +38,24 @@ export const createSuscripcion = async (req, res) => {
         const fecha = new Date()
         const nuevaSuscripcion = new Suscripcion({email, fecha})
         await nuevaSuscripcion.save()
+        const enviarMail = async () => {
+            const transporter = nodemailer.createTransport({
+                host: 'smtp.hostinger.com',
+                port: 465,
+                secure: true,
+                auth: {
+                    user: process.env.EMAIL,
+                    password: process.env.EMAIL_PASSWORD
+                }
+            })
+            await transporter.sendMail({
+                from: '"Jorge Tapia" <contacto@blaspod.cl>',
+                to: email,
+                subject: '¡Te damos la Bienvenida a Blaspos Store! Nos hace muy felices tenerte con nosotros',
+                html: '<div style="display: flex; margin: 20px;"><div style="margin: auto; width: 600px;"><div style="display: flex; border-bottom: 1px solid #caccd6; padding-bottom: 10px;"><a style="margin: auto;" href="https://blaspod.cl/"><img src="https://res.cloudinary.com/blasspod/image/upload/v1664841659/blaspod/ouxxwsmqodpemvffqs7b.png" style="width: 300px;" /></a></div><div style="border-bottom: 1px solid #caccd6; padding-bottom: 30px;"><h1 style="text-align: center; font-family: poppins, sans-serif; margin-bottom: -10px; padding-bottom: 0px;">Te damos las gracias por unirte a nuestra lista</h1><p style="text-align: center; font-family: poppins, sans-serif;">Ahora seras el primero en enterarte de increíbles ofertas, sorteos y ademas podras tener acceso privilegiado a ofertas exclusivas de la tienda.</p><div style="display: flex;"><a style="margin: auto; background-color: #303237; color: #fff; padding: 8px 35px; border-radius: 5px; font-family: poppins, sans-serif; text-decoration: none;" href="https://blaspod.cl/">Visitar tienda</a></div></div><div style="display: flex; justify-content: space-between;"><div style="padding: 0px 5px; width: 50%;"><p style="font-family: poppins, sans-serif; margin-bottom: -10px;">Blaspod Store</p><p style="font-family: poppins, sans-serif; margin-bottom: -10px;">+56996150056</p><p style="font-family: poppins, sans-serif; margin-bottom: -10px;">Comandante chacon 6076, Quinta Normal</p></div><div style="padding: 0px 5px; width: 50%; display: flex; flex-direction: column; gap: 10px; margin-top: 13px;"><a style="font-family: poppins, sans-serif; margin-bottom: -10px; text-align: end;" href="https://blaspod.cl">Pagina web</a><a style="font-family: poppins, sans-serif; margin-bottom: -10px; text-align: end;" href="https://facebook.com/blaspod">Facebook</a><a style="font-family: poppins, sans-serif; margin-bottom: -10px; text-align: end;" href="https://instagram.com/blaspod">Instagram</a><a style="font-family: poppins, sans-serif; margin-bottom: -10px; text-align: end;" href="https://wa.me/message/4H6Q4RGYT4FHK1">Whatsapp</a><a style="font-family: poppins, sans-serif; margin-bottom: -10px; text-align: end;" href="https://blaspod.cl">Cancelar Suscripción</a></div></div></div></div>'
+            })
+        }
+        enviarMail()
         return res.json(nuevaSuscripcion)
     } catch (error) {
         return res.status(500).json({message: error.message})
