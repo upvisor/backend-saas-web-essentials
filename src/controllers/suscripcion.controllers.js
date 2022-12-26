ea5d55635d8034fad396c5c407ac036e1a2f77be
Jorge Tapia <jorge504tapia@gmail.com>
2022-12-26 15:41:46 -0300

    mail configuration 6

--- src/controllers/suscripcion.controllers.js
+++ src/controllers/suscripcion.controllers.js
@@ -38,22 +38,51 @@ export const createSuscripcion = async (req, res) => {
         const fecha = new Date()
         const nuevaSuscripcion = new Suscripcion({email, fecha})
         await nuevaSuscripcion.save()
-        const enviarMail = async () => {
-            const transporter = nodemailer.createTransport({
-                service: 'gmail',
+        async function main() {
+            let transporter = nodemailer.createTransport({
+                host: "smtp.hostinger.com",
+                port: 465,
+                secure: true,
                 auth: {
-                    user: process.env.EMAIL,
-                    password: process.env.EMAIL_PASSWORD
-                }
+                    user: 'contacto@blaspod.cl',
+                    pass: 'Estevan5966@',
+                },
             })
             await transporter.sendMail({
-                from: 'jorge504tapia@gmail.com',
+                from: '"Blaspod Store" <contacto@blaspod.cl>',
                 to: email,
-                subject: '<p>¡Te damos la Bienvenida a Blaspod Store! Nos hace muy felices tenerte con nosotros</p>',
-                html: '<p>Hola</p>'
+                subject: "¡Te damos la bienvenida a Blaspod Store! Nos hace muy felices tenerte con nosotros",
+                html: `<div style="display: flex; margin: 20px;">
+                <div style="margin: auto; width: 600px;">
+                    <div style="display: flex; border-bottom: 1px solid #caccd6; padding-bottom: 10px;">
+                        <a style="margin: auto;" href="https://blaspod.cl/"><img src="https://res.cloudinary.com/blasspod/image/upload/v1664841659/blaspod/ouxxwsmqodpemvffqs7b.png" style="width: 270px;" /></a>
+                    </div>
+                    <div style="border-bottom: 1px solid #caccd6; padding-bottom: 30px;">
+                        <h1 style="text-align: center; font-family: poppins, sans-serif; padding-bottom: 0px; font-weight: 500; font-size: 30px;">Te damos las gracias por unirte a nuestra lista</h1>
+                        <p style="text-align: center; font-family: poppins, sans-serif; color: #000000; font-size: 16px; font-weight: 300;">Ahora seras el primero en enterarte de increíbles ofertas, sorteos y ademas podras tener acceso privilegiado a ofertas exclusivas de la tienda.</p>
+                        <div style="display: flex;">
+                            <a style="margin: auto; background-color: #303237; color: #fff; padding: 8px 35px; border-radius: 5px; font-family: poppins, sans-serif; text-decoration: none; font-size: 16px;" href="https://blaspod.cl/">Visitar tienda</a>
+                        </div>
+                    </div>
+                    <div style="display: flex; justify-content: space-between;">
+                        <div style="padding: 0px 5px; width: 50%;">
+                            <p style="font-family: poppins, sans-serif; font-size: 15px;">Blaspod Store</p>
+                            <p style="font-family: poppins, sans-serif; font-size: 15px;">+56996150056</p>
+                            <p style="font-family: poppins, sans-serif; font-size: 15px;">Comandante chacon 6076, Quinta Normal</p>
+                        </div>
+                        <div style="padding: 0px 5px; width: 50%; margin-top: 10px; text-align: end">
+                            <a style="font-family: poppins, sans-serif; font-size: 15px; display: block;" href="https://blaspod.cl">Pagina web</a>
+                            <a style="font-family: poppins, sans-serif; font-size: 15px; display: block;" href="https://facebook.com/blaspod">Facebook</a>
+                            <a style="font-family: poppins, sans-serif; font-size: 15px; display: block;" href="https://instagram.com/blaspod">Instagram</a>
+                            <a style="font-family: poppins, sans-serif; font-size: 15px; display: block;" href="https://wa.me/message/4H6Q4RGYT4FHK1">Whatsapp</a>
+                            <a style="font-family: poppins, sans-serif; font-size: 15px; display: block;" href="https://blaspod.cl">Cancelar Suscripción</a>
+                        </div>
+                    </div>
+                </div>
+            </div>`
             })
         }
-        enviarMail()
+        main().catch(console.error)
         return res.json(nuevaSuscripcion)
     } catch (error) {
         return res.status(500).json({message: error.message})
