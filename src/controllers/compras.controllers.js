3c14b02ed339f577177f02f510868d799935c250
Jorge Tapia <jorge504tapia@gmail.com>
2022-12-27 13:15:34 -0300

    mail compra configuration

--- src/controllers/compras.controllers.js
+++ src/controllers/compras.controllers.js
@@ -1,5 +1,6 @@
 import Compra from '../models/Compra.js'
 import bizSdk from 'facebook-nodejs-business-sdk'
+import nodemailer from 'nodemailer'
 
 export const createCompra = async (req, res) => {
     try {
@@ -129,6 +130,51 @@ export const updateCompra = async (req, res) => {
                         console.error('Error: ', err)
                     }
                 )
+            async function main() {
+                let transporter = nodemailer.createTransport({
+                    host: "smtp.hostinger.com",
+                    port: 465,
+                    secure: true,
+                    auth: {
+                        user: 'contacto@blaspod.cl',
+                        pass: 'Estevan5966@',
+                    },
+                })
+                await transporter.sendMail({
+                    from: '"Blaspod Store" <contacto@blaspod.cl>',
+                    to: compra.email,
+                    subject: `¡${compra.nombre} Tu compra ha ha sido generada y pagada con exito!`,
+                    html: `<div style="display: flex; margin: 20px;">
+                    <div style="margin: auto; width: 600px;">
+                        <div style="display: flex; border-bottom: 1px solid #caccd6; padding-bottom: 10px;">
+                            <a style="margin: auto;" href="https://blaspod.cl/"><img src="https://res.cloudinary.com/blasspod/image/upload/v1664841659/blaspod/ouxxwsmqodpemvffqs7b.png" style="width: 270px;" /></a>
+                        </div>
+                        <div style="border-bottom: 1px solid #caccd6; padding-bottom: 30px;">
+                            <h1 style="text-align: center; font-family: poppins, sans-serif; padding-bottom: 0px; font-weight: 500; font-size: 30px;">Te damos las gracias por confiar en nosotros</h1>
+                            <p style="text-align: center; font-family: poppins, sans-serif; color: #000000; font-size: 16px; font-weight: 300;">¡Hola ${compra.nombre}! Te queriamos comentar que tu compra ya ha sido generada y pagada con exito, cualquier actualización de tu pedido sera informado por este medio.</p>
+                            <div style="display: flex;">
+                                <a style="margin: auto; background-color: #303237; color: #fff; padding: 8px 35px; border-radius: 5px; font-family: poppins, sans-serif; text-decoration: none; font-size: 16px;" href="https://blaspod.cl/#/contacto">Contactate con nosotros</a>
+                            </div>
+                        </div>
+                        <div style="display: flex; justify-content: space-between;">
+                            <div style="padding: 0px 5px; width: 50%;">
+                                <p style="font-family: poppins, sans-serif; font-size: 15px;">Blaspod Store</p>
+                                <p style="font-family: poppins, sans-serif; font-size: 15px;">+56996150056</p>
+                                <p style="font-family: poppins, sans-serif; font-size: 15px;">Comandante chacon 6076, Quinta Normal</p>
+                            </div>
+                            <div style="padding: 0px 5px; width: 50%; margin-top: 10px; text-align: end">
+                                <a style="font-family: poppins, sans-serif; font-size: 15px; display: block;" href="https://blaspod.cl">Pagina web</a>
+                                <a style="font-family: poppins, sans-serif; font-size: 15px; display: block;" href="https://facebook.com/blaspod">Facebook</a>
+                                <a style="font-family: poppins, sans-serif; font-size: 15px; display: block;" href="https://instagram.com/blaspod">Instagram</a>
+                                <a style="font-family: poppins, sans-serif; font-size: 15px; display: block;" href="https://wa.me/message/4H6Q4RGYT4FHK1">Whatsapp</a>
+                                <a style="font-family: poppins, sans-serif; font-size: 15px; display: block;" href="https://blaspod.cl">Cancelar Suscripción</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>`
+                })
+            }
+            main().catch(console.error)
         }
         return res.send(updateCompra)
     } catch (error) {
