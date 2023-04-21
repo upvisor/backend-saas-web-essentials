24b2ecea650d7d7ddae68d8ed6aaeb63cb37e668
Jorge <admin@maaide.com>
2023-04-21 14:09:52 -0400

    actualization general

--- src/controllers/client.controllers.js
+++ src/controllers/client.controllers.js
@@ -23,3 +23,12 @@ export const getClients = async (req, res) => {
     return res.status(500).json({message: error.message})
   }
 }
+
+export const updateClient = async (req, res) => {
+  try {
+    const updateClient = await Client.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    return res.send(updateClient)
+  } catch (error) {
+    return res.status(500).json({message: error.message})
+  }
+}
--- src/routes/client.routers.js
+++ src/routes/client.routers.js
@@ -1,10 +1,12 @@
 import {Router} from 'express'
-import { createClient, getClients } from '../controllers/client.controllers.js'
+import { createClient, getClients, updateClient } from '../controllers/client.controllers.js'
 
 const router = Router()
 
 router.post('/clients', createClient)
 
 router.get('/clients', getClients)
+
+router.put('/clients', updateClient)
 
 export default router
