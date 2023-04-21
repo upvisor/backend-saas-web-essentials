9697a7034803f1bc66aae678b05da78294529071
Jorge <admin@maaide.com>
2023-04-21 12:38:40 -0400

    actualization general

--- src/models/Client.js
+++ src/models/Client.js
@@ -4,6 +4,11 @@ const ClientSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },
   firstName: { type: String },
   lastName: { type: String },
+  phone: { type: Number },
+  address: { type: String },
+  departament: { type: String },
+  region: { type: String },
+  city: { type: String },
   tags: [{ type: String }]
 })
 
