e50e0138417bfd67a70c2e4d0848ae9bb54703eb
Jorge <admin@maaide.com>
2023-05-12 13:53:04 -0400

    actualization general

--- src/routes/webhook.routes.js
+++ src/routes/webhook.routes.js
@@ -3,9 +3,8 @@ import {Router} from 'express'
 const router = Router()
 
 router.get('/webhook', function(req, res) {
+    console.log('hola')
     if (req.query['hub.verify_token'] === 'maaide_token') {
-        console.log(req.query)
-        console.log(req.body)
         res.send(req.query['hub.challenge'])
     } else {
         res.send('No tienes permisos.')
