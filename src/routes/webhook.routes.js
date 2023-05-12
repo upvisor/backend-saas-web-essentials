1dbd55f17d20cd0e5a425b9ccd01b9dc4a4d9085
Jorge <admin@maaide.com>
2023-05-12 11:33:21 -0400

    actualization general

--- src/routes/webhook.routes.js
+++ src/routes/webhook.routes.js
@@ -3,7 +3,7 @@ import {Router} from 'express'
 const router = Router()
 
 router.get('/webhook', function(req, res) {
-    if (req.query['hub.verify_token'] === 'pugpizza_token') {
+    if (req.query['hub.verify_token'] === 'maaide_token') {
         res.send(req.query['hub.challenge'])
     } else {
         res.send('Pug Pizza no tienes permisos.')
