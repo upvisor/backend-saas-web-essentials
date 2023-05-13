4e818bf187dda436d48c00df97ab4d26fd545586
Jorge <admin@maaide.com>
2023-05-13 12:56:54 -0400

    actualization general

--- src/controllers/webhook.controllers.js
+++ src/controllers/webhook.controllers.js
@@ -11,7 +11,8 @@ export const createWebhook = (req, res) => {
 }
 
 export const getMessage = async (req, res) => {
-   if (req.body?.entry[0]?.changes[0]?.value?.messages[0]?.text?.body) {
+    if (req.body?.entry && req.body.entry[0]?.changes && req.body.entry[0].changes[0]?.value?.messages && 
+        req.body.entry[0].changes[0].value.messages[0]?.text && req.body.entry[0].changes[0].value.messages[0].text.body) {    
         const message = req.body.entry[0].changes[0].value.messages[0].text.body
         const number = req.body.entry[0].changes[0].value.messages[0].from
         const configuration = new Configuration({
