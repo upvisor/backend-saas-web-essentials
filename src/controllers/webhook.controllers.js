6d2735334670435a80a755fbea7ea37a5d3b6bfb
Jorge <admin@maaide.com>
2023-05-16 10:12:36 -0400

    actualization general

--- src/controllers/webhook.controllers.js
+++ src/controllers/webhook.controllers.js
@@ -17,59 +17,76 @@ export const getMessage = async (req, res) => {
             req.body.entry[0].changes[0].value.messages[0]?.text && req.body.entry[0].changes[0].value.messages[0].text.body) {    
             const message = req.body.entry[0].changes[0].value.messages[0].text.body
             const number = req.body.entry[0].changes[0].value.messages[0].from
-            const configuration = new Configuration({
-                organization: "org-s20w0nZ3MxE2TSG8LAAzz4TO",
-                apiKey: process.env.OPENAI_API_KEY,
-            });
-            const openai = new OpenAIApi(configuration)
-            const responseCategorie = await openai.createCompletion({
-                model: "text-davinci-003",
-                prompt: `Con las siguientes categorias: saludo, productos, envios, horarios, seguridad, garantia, promociones y devoluciones. Cuales encajan mejor con la siguiente pregunta: ${message}`,
-                temperature: 0,
-                max_tokens: 100
-            })
-            const categories = responseCategorie.data.choices[0].text.toLowerCase()
-            let information = ''
-            if (categories.includes('productos')) {
-                const products = await Product.find().select('name description stock price beforePrice variations -_id').lean()
-                information = `${information}. ${JSON.stringify(products)}`
-            }
-            const messages = await WhatsappMessage.find({phone: number}).select('-phone -_id').lean()
-            const ultimateMessage = messages.reverse()
-            let structure
-            if (ultimateMessage.length) {
-                structure = [
-                    {"role": "system", "content": `Eres un asistente llamado Maaibot de la tienda Maaide, responde la siguiente pregunta utilizando unicamente la siguiente informacion: ${information}`},
-                    {"role": "user", "content": ultimateMessage[0].message},
-                    {"role": "assistant", "content": ultimateMessage[0].response},
-                    {"role": "user", "content": message}
-                ]
-            } else {
-                structure = [
-                    {"role": "system", "content": `Eres un asistente llamado Maaibot de la tienda Maaide, responde la siguiente pregunta utilizando unicamente la siguiente informacion: ${information}`},
-                    {"role": "user", "content": message}
-                ]
-            }
-            const responseChat = await openai.createChatCompletion({
-                model: "gpt-3.5-turbo",
-                temperature: 0,
-                messages: structure
-            })
-            const responseMessage = responseChat.data.choices[0].message.content
-            await axios.post('https://graph.facebook.com/v16.0/108940562202993/messages', {
+            if (message.toLowerCase() === 'agente') {
+                await axios.post('https://graph.facebook.com/v16.0/108940562202993/messages', {
                 "messaging_product": "whatsapp",
                 "to": number,
                 "type": "text",
-                "text": {"body": responseMessage}
-            }, {
-                headers: {
-                    'Content-Type': 'application/json',
-                    "Authorization": `Bearer ${process.env.WHATSAPP_TOKEN}`
+                "text": {"body": "¡Perfecto! Ahora te estamos transfiriendo con un agente"}
+                }, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        "Authorization": `Bearer ${process.env.WHATSAPP_TOKEN}`
+                    }
+                })
+                const newMessage = new WhatsappMessage({phone: number, message: message, response: responseMessage, agent: true})
+                await newMessage.save()
+                return res.sendStatus(200)
+            } else {
+                const configuration = new Configuration({
+                    organization: "org-s20w0nZ3MxE2TSG8LAAzz4TO",
+                    apiKey: process.env.OPENAI_API_KEY,
+                })
+                const openai = new OpenAIApi(configuration)
+                const responseCategorie = await openai.createCompletion({
+                    model: "text-davinci-003",
+                    prompt: `Con las siguientes categorias: saludo, productos, envios, horarios, seguridad, garantia, promociones y devoluciones. Cuales encajan mejor con la siguiente pregunta: ${message}`,
+                    temperature: 0,
+                    max_tokens: 100
+                })
+                const categories = responseCategorie.data.choices[0].text.toLowerCase()
+                let information = ''
+                if (categories.includes('productos')) {
+                    const products = await Product.find().select('name description stock price beforePrice variations -_id').lean()
+                    information = `${information}. ${JSON.stringify(products)}`
                 }
-            })
-            const newMessage = new WhatsappMessage({phone: number, message: message, response: responseMessage})
-            await newMessage.save()
-            return res.sendStatus(200)
+                const messages = await WhatsappMessage.find({phone: number}).select('-phone -_id').lean()
+                const ultimateMessage = messages.reverse()
+                let structure
+                if (ultimateMessage.length) {
+                    structure = [
+                        {"role": "system", "content": `Eres un asistente llamado Maaibot de la tienda Maaide, si no puedes responder la pregunta con la información dada, indica que pueden hablar con un agente escribiendo "agente" en el chat, responde la siguiente pregunta utilizando únicamente la siguiente información: ${information}`},
+                        {"role": "user", "content": ultimateMessage[0].message},
+                        {"role": "assistant", "content": ultimateMessage[0].response},
+                        {"role": "user", "content": message}
+                    ]
+                } else {
+                    structure = [
+                        {"role": "system", "content": `Eres un asistente llamado Maaibot de la tienda Maaide, responde la siguiente pregunta utilizando unicamente la siguiente informacion: ${information}`},
+                        {"role": "user", "content": message}
+                    ]
+                }
+                const responseChat = await openai.createChatCompletion({
+                    model: "gpt-3.5-turbo",
+                    temperature: 0,
+                    messages: structure
+                })
+                const responseMessage = responseChat.data.choices[0].message.content
+                await axios.post('https://graph.facebook.com/v16.0/108940562202993/messages', {
+                    "messaging_product": "whatsapp",
+                    "to": number,
+                    "type": "text",
+                    "text": {"body": responseMessage}
+                }, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        "Authorization": `Bearer ${process.env.WHATSAPP_TOKEN}`
+                    }
+                })
+                const newMessage = new WhatsappMessage({phone: number, message: message, response: responseMessage, agent: false})
+                await newMessage.save()
+                return res.sendStatus(200)
+            }
         } else {
             return res.sendStatus(404)
         }
