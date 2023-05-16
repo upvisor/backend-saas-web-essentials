0e8d8985f4cfa06be834b100f1766946ba8a9d86
Jorge <admin@maaide.com>
2023-05-16 11:05:29 -0400

    actualization general

--- src/controllers/webhook.controllers.js
+++ src/controllers/webhook.controllers.js
@@ -18,76 +18,59 @@ export const getMessage = async (req, res) => {
             req.body.entry[0].changes[0].value.messages[0]?.text && req.body.entry[0].changes[0].value.messages[0].text.body) {    
             const message = req.body.entry[0].changes[0].value.messages[0].text.body
             const number = req.body.entry[0].changes[0].value.messages[0].from
-            if (message.toLowerCase() === 'agente') {
-                await axios.post('https://graph.facebook.com/v16.0/108940562202993/messages', {
-                    "messaging_product": "whatsapp",
-                    "to": number,
-                    "type": "text",
-                    "text": {"body": "¡Perfecto! Ahora te estamos transfiriendo con un agente"}
-                }, {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        "Authorization": `Bearer ${process.env.WHATSAPP_TOKEN}`
-                    }
-                })
-                const newMessage = new WhatsappMessage({phone: number, message: message, response: responseMessage, agent: true})
-                await newMessage.save()
-                return res.sendStatus(200)
-            } else {
-                const configuration = new Configuration({
-                    organization: "org-s20w0nZ3MxE2TSG8LAAzz4TO",
-                    apiKey: process.env.OPENAI_API_KEY,
-                })
-                const openai = new OpenAIApi(configuration)
-                const responseCategorie = await openai.createCompletion({
-                    model: "text-davinci-003",
-                    prompt: `Con las siguientes categorias: saludo, productos, envios, horarios, seguridad, garantia, promociones y devoluciones. Cuales encajan mejor con la siguiente pregunta: ${message}`,
-                    temperature: 0,
-                    max_tokens: 100
-                })
-                const categories = responseCategorie.data.choices[0].text.toLowerCase()
-                let information = ''
-                if (categories.includes('productos')) {
-                    const products = await Product.find().select('name description stock price beforePrice variations -_id').lean()
-                    information = `${information}. ${JSON.stringify(products)}`
-                }
-                const messages = await WhatsappMessage.find({phone: number}).select('-phone -_id').lean()
-                const ultimateMessage = messages.reverse()
-                let structure
-                if (ultimateMessage.length) {
-                    structure = [
-                        {"role": "system", "content": `Eres un asistente llamado Maaibot de la tienda Maaide, es sumamente importante que si no tienes la informacion para responder la pregunta, indiques que pueden hablar con un agente escribiendo "agente" en el chat, la informacion es la siguiente: ${information}`},
-                        {"role": "user", "content": ultimateMessage[0].message},
-                        {"role": "assistant", "content": ultimateMessage[0].response},
-                        {"role": "user", "content": message}
-                    ]
-                } else {
-                    structure = [
-                        {"role": "system", "content": `Eres un asistente llamado Maaibot de la tienda Maaide, es sumamente importante que si no tienes la informacion para responder la pregunta, indiques que pueden hablar con un agente escribiendo "agente" en el chat, la informacion es la siguiente: ${information}`},
-                        {"role": "user", "content": message}
-                    ]
-                }
-                const responseChat = await openai.createChatCompletion({
-                    model: "gpt-3.5-turbo",
-                    temperature: 0,
-                    messages: structure
-                })
-                const responseMessage = responseChat.data.choices[0].message.content
-                await axios.post('https://graph.facebook.com/v16.0/108940562202993/messages', {
-                    "messaging_product": "whatsapp",
-                    "to": number,
-                    "type": "text",
-                    "text": {"body": responseMessage}
-                }, {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        "Authorization": `Bearer ${process.env.WHATSAPP_TOKEN}`
-                    }
-                })
-                const newMessage = new WhatsappMessage({phone: number, message: message, response: responseMessage, agent: false})
-                await newMessage.save()
-                return res.sendStatus(200)
+            const configuration = new Configuration({
+                organization: "org-s20w0nZ3MxE2TSG8LAAzz4TO",
+                apiKey: process.env.OPENAI_API_KEY,
+            })
+            const openai = new OpenAIApi(configuration)
+            const responseCategorie = await openai.createCompletion({
+                model: "text-davinci-003",
+                prompt: `Con las siguientes categorias: saludo, productos, envios, horarios, seguridad, garantia, promociones y devoluciones. Cuales encajan mejor con la siguiente pregunta: ${message}`,
+                temperature: 0,
+                max_tokens: 100
+            })
+            const categories = responseCategorie.data.choices[0].text.toLowerCase()
+            let information = ''
+            if (categories.includes('productos')) {
+                const products = await Product.find().select('name description stock price beforePrice variations -_id').lean()
+                information = `${information}. ${JSON.stringify(products)}`
             }
+            const messages = await WhatsappMessage.find({phone: number}).select('-phone -_id').lean()
+            const ultimateMessage = messages.reverse()
+            let structure
+            if (ultimateMessage.length) {
+                structure = [
+                    {"role": "system", "content": `Eres un asistente llamado Maaibot de la tienda Maaide, es sumamente importante que si no tienes la informacion para responder la pregunta, indiques que pueden hablar con un agente escribiendo "agente" en el chat, la informacion es la siguiente: ${information}`},
+                    {"role": "user", "content": ultimateMessage[0].message},
+                    {"role": "assistant", "content": ultimateMessage[0].response},
+                    {"role": "user", "content": message}
+                ]
+            } else {
+                structure = [
+                    {"role": "system", "content": `Eres un asistente llamado Maaibot de la tienda Maaide, es sumamente importante que si no tienes la informacion para responder la pregunta, indiques que pueden hablar con un agente escribiendo "agente" en el chat, la informacion es la siguiente: ${information}`},
+                    {"role": "user", "content": message}
+                ]
+            }
+            const responseChat = await openai.createChatCompletion({
+                model: "gpt-3.5-turbo",
+                temperature: 0,
+                messages: structure
+            })
+            const responseMessage = responseChat.data.choices[0].message.content
+            await axios.post('https://graph.facebook.com/v16.0/108940562202993/messages', {
+                "messaging_product": "whatsapp",
+                "to": number,
+                "type": "text",
+                "text": {"body": responseMessage}
+            }, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    "Authorization": `Bearer ${process.env.WHATSAPP_TOKEN}`
+                }
+            })
+            const newMessage = new WhatsappMessage({phone: number, message: message, response: responseMessage, agent: false})
+            await newMessage.save()
+            return res.sendStatus(200)
         } else {
             return res.sendStatus(200)
         }
