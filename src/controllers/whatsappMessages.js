052a938f7c2b0c2e7cca7cf3aa9d3b71109d74d7
Jorge <admin@maaide.com>
2023-05-17 19:44:55 -0400

    actualization general

--- src/controllers/whatsappMessages.js
+++ src/controllers/whatsappMessages.js
@@ -2,7 +2,7 @@ import WhatsappChat from '../models/WhatsappChat.js'
 
 export const getMessages = async (req, res) => {
     try {
-        const messages = await WhatsappChat.find().select('-_id').lean()
+        const messages = await WhatsappChat.find().lean()
         return res.send(messages)
     } catch (error) {
         return res.status(500).json({message: error.message})
