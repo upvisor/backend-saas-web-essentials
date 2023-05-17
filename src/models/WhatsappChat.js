2e6d063d6b80377f2ad9e9c807575d786909afff
Jorge <admin@maaide.com>
2023-05-17 19:11:11 -0400

    actualization general

--- src/models/WhatsappChat.js
+++ src/models/WhatsappChat.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const WhatsappMessageSchema = mongoose.Schema({
     phone: { type: Number, required: true },
-    message: { type: String, required: true },
+    message: { type: String },
     response: { type: String },
     agent: { type: Boolean, required: true }
 }, {
