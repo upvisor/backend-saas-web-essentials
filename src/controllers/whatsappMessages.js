918cfa871dfd332e6a045f0c1ee47919b4076f10
Jorge <admin@maaide.com>
2023-05-18 13:54:13 -0400

    actualization general

--- src/controllers/whatsappMessages.js
+++ src/controllers/whatsappMessages.js
@@ -2,8 +2,20 @@ import WhatsappChat from '../models/WhatsappChat.js'
 
 export const getMessages = async (req, res) => {
     try {
-        const messages = await WhatsappChat.find().lean()
-        return res.send(messages)
+        const messages = await WhatsappChat.find().select('-message -response').lean()
+        const filter = messages.filter(message => message.agent === true)
+        const phoneNumbers = filter.map(item => item.phone)
+        const uniquePhoneNumbersSet = new Set(phoneNumbers)
+        const uniquePhoneNumbersArray = [...uniquePhoneNumbersSet]
+        let chats = []
+        await Promise.all(
+            uniquePhoneNumbersArray.map(async phone => {
+                const messagesPhone = await WhatsappChat.find({phone: phone}).lean()
+                console.log(messagesPhone)
+                chats = chats.concat(messagesPhone)
+            })
+        )
+        return res.send(chats)
     } catch (error) {
         return res.status(500).json({message: error.message})
     }
