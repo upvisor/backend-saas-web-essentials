fbeb5204a393de7c1c607aeb24b8299befa5604e
Jorge <admin@maaide.com>
2023-05-21 19:52:00 -0400

    actualization general

--- src/models/MessengerChat.js
+++ src/models/MessengerChat.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 const MessengerMessageSchema = mongoose.Schema({
-    messegerId: { type: string, required: true },
+    messegerId: { type: String, required: true },
     message: { type: String },
     response: { type: String },
     agent: { type: Boolean, required: true }
