1a0d90dea64291b923c2a738fd16d01932124268
Jorge <admin@maaide.com>
2023-05-23 16:06:57 -0400

    actualization general

---
+++ src/models/InstagramChat.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+
+const InstagramMessageSchema = mongoose.Schema({
+    instagramId: { type: String, required: true },
+    message: { type: String },
+    response: { type: String },
+    agent: { type: Boolean, required: true }
+}, {
+    timestamps: true
+})
+
+const InstagramMessage = mongoose.models.InstagramMessage || mongoose.model('InstagramMessage', InstagramMessageSchema)
+
+export default InstagramMessage
