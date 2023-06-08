08d50396f7ee1282c2c76629127fdf020e5f6034
Jorge <admin@maaide.com>
2023-06-08 13:35:43 -0400

    actualization general

--- src/controllers/instagramMessage.controllers.js
+++ src/controllers/instagramMessage.controllers.js
@@ -45,7 +45,7 @@ export const getMessagesInstagram = async (req, res) => {
 
 export const createMessage = async (req, res) => {
     try {
-        await axios.post(`https://graph.facebook.com/v16.0/106714702292810/messages?access_token=${process.env.INSTAGRAM_TOKEN}`, {
+        await axios.post(`https://graph.facebook.com/v16.0/106714702292810/messages?access_token=${process.env.MESSENGER}`, {
             "recipient": {
                 "id": req.body.sender
             },
