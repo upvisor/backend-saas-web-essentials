7be4bcce5762ea7d2720dc9aceef541dd9af0311
Jorge <admin@maaide.com>
2023-06-13 14:03:29 -0400

    actualization general

--- src/controllers/aiDescriptionProduct.controllers.js
+++ src/controllers/aiDescriptionProduct.controllers.js
@@ -2,7 +2,7 @@ import { Configuration, OpenAIApi } from "openai"
 
 export const createDescription = async (req, res) => {
     try {
-        const { description } = req.body
+        const { description, type } = req.body
         const configuration = new Configuration({
             organization: "org-s20w0nZ3MxE2TSG8LAAzz4TO",
             apiKey: process.env.OPENAI_API_KEY,
@@ -10,7 +10,7 @@ export const createDescription = async (req, res) => {
         const openai = new OpenAIApi(configuration)
         const response = await openai.createCompletion({
             model: "text-davinci-003",
-            prompt: `Respóndeme como un experto en copywriting y seo especializado en E-commerce con mas de 15 años de experiencia. Quiero que redactes una descripción para un producto de un E-commerce, habla acerca del producto con la palabra "nuestro", no repitas partes de la descripción y no pongas "en resumen" en el parrafo final. El producto es: ${description}`,
+            prompt: `Respóndeme como un experto en copywriting y seo especializado en E-commerce con mas de 15 años de experiencia. Quiero que redactes una descripción con un tono ${type} para un producto de un E-commerce, habla acerca del producto con la palabra "nuestro", no repitas partes de la descripción y no pongas "en resumen" en el parrafo final. El producto es: ${description}`,
             max_tokens: 1000,
             temperature: 0
         })
