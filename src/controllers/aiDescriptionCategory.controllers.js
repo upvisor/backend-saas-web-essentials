a98338913f978a5eb3f081e8c31e9df115abe322
Jorge <admin@maaide.com>
2023-06-14 11:21:38 -0400

    actualization general

--- src/controllers/aiDescriptionCategory.controllers.js
+++ src/controllers/aiDescriptionCategory.controllers.js
@@ -2,7 +2,7 @@ import { Configuration, OpenAIApi } from "openai"
 
 export const createDescription = async (req, res) => {
     try {
-        const { description } = req.body
+        const { description, type } = req.body
         const configuration = new Configuration({
             organization: "org-s20w0nZ3MxE2TSG8LAAzz4TO",
             apiKey: process.env.OPENAI_API_KEY,
@@ -10,7 +10,7 @@ export const createDescription = async (req, res) => {
         const openai = new OpenAIApi(configuration)
         const response = await openai.createCompletion({
             model: "text-davinci-003",
-            prompt: `Respóndeme como un experto en copywriting y seo especializado en E-commerce con mas de 15 años de experiencia. Quiero que redactes una descripción corta para una categoria de un E-commerce. La categoria es: ${description}`,
+            prompt: `Respóndeme como un experto en copywriting y seo especializado en E-commerce con mas de 15 años de experiencia. Quiero que redactes una descripción corta para una categoria de un E-commerce con un tono ${type}. La categoria es: ${description}`,
             max_tokens: 1000,
             temperature: 0
         })
