e745c773a82337f21f88125bd1b847aceaf47e78
Jorge <admin@maaide.com>
2023-06-17 13:12:29 -0400

    actualization general

---
+++ src/controllers/capaigns.controllers.js
@@ -0,0 +1,9 @@
+import Email from '../models/Email.js'
+
+export const createCampaign = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        return res.status(500).json({message: error.message})
+    }
+}
---
+++ src/models/Email.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose'
+
+const EmailSchema = new mongoose.Schema({
+    address: [],
+    affair: { type: String, required: true },
+    summary: { type: String, required: true },
+    title: { type: String },
+    paragraph: { type: String },
+    buttonText: { type: String },
+    url: { type: String },
+    date: { type: Date, required: true }
+}, {
+    timestamps: true
+})
+
+const Email = mongoose.models.Email || mongoose.model('Email', EmailSchema)
+
+export default Email
