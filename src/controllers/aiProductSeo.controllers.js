5fad406f942fb61f1c540366a49e4d059d1d2405
Jorge <admin@maaide.com>
2023-06-17 14:31:04 -0400

    actualization general

--- src/controllers/aiProductSeo.controllers.js
+++ src/controllers/aiProductSeo.controllers.js
@@ -8,21 +8,25 @@ export const createSeo = async (req, res) => {
             apiKey: process.env.OPENAI_API_KEY,
         });
         const openai = new OpenAIApi(configuration)
-        const responseTitle = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: `Respóndeme como un experto en copywriting y seo especializado en E-commerce con mas de 15 años de experiencia. Quiero que redactes un meta título con un tono ${type} para un producto de un E-commerce. Finaliza el meta titulo con "| Blaspod". El producto es: ${description}`,
-            max_tokens: 1000,
-            temperature: 0
+        const responseTitle = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            temperature: 1,
+            messages: [
+                {"role": "system", "content": `Ponte en la piel de un experto SEO especializado en ecommerce, quiero que redactes un meta titulo para la pagina de producto con un tono ${type}`},
+                {"role": "user", "content": `El producto es el siguiente: ${description}`}
+            ]
         })
-        const responseDescription = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: `Respóndeme como un experto en copywriting y seo especializado en E-commerce con mas de 15 años de experiencia. Quiero que redactes una meta descripción con un tono ${type} para un producto de un E-commerce, habla acerca del producto con la palabra "nuestro". El producto es: ${description}`,
-            max_tokens: 1000,
-            temperature: 0
+        const responseDescription = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            temperature: 1,
+            messages: [
+                {"role": "system", "content": `Ponte en la piel de un experto SEO especializado en ecommerce, quiero que redactes una meta descripción para la pagina de producto de maximo 140 caracteres con un tono ${type}`},
+                {"role": "user", "content": `El producto es el siguiente: ${description}`}
+            ]
         })
         return res.json({
-            title: responseTitle.data.choices,
-            description: responseDescription.data.choices
+            title: responseTitle.data.choices[0].message.content,
+            description: responseDescription.data.choices[0].message.content
         })
     } catch (error) {
         return res.status(500).json({message: error.message})
