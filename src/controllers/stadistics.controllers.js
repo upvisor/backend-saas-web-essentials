9e76a1eec9a66fc00790840029933ff4637e408e
Jorge <admin@maaide.com>
2023-06-19 14:41:03 -0400

    actualization general

--- src/controllers/stadistics.controllers.js
+++ src/controllers/stadistics.controllers.js
@@ -2,7 +2,6 @@ import AddCart from '../models/AddCart.js'
 import ViewContent from '../models/ViewContent.js'
 import Information from '../models/Information.js'
 import Sell from '../models/Sell.js'
-import Subscribe from '../models/Subscribe.js'
 import Contact from '../models/Contact.js'
 
 export const getStadistics = async (req, res) => {
@@ -11,7 +10,6 @@ export const getStadistics = async (req, res) => {
     const addCarts = await AddCart.find()
     const informations = await Information.find()
     const sells = await Sell.find()
-    const subscriptions = await Subscribe.find()
     const contacts = await Contact.find()
     return res.send([viewContents, addCarts, informations, sells, subscriptions, contacts])
   } catch (error) {
