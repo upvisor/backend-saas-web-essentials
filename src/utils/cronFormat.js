81ebbd9d1c11d945b74b09054e93ea191dd1f0ff
Jorge <contacto@maaide.com>
2023-06-20 19:06:34 -0400

    actualization general

--- src/utils/cronFormat.js
+++ src/utils/cronFormat.js
@@ -2,8 +2,8 @@ export function formatDateToCron(date) {
     const minute = date.getMinutes()
     const hour = date.getHours()
     const dayOfMonth = date.getDate()
-    const month = date.getMonth() + 1 // Los meses en JavaScript comienzan desde 0
-    const dayOfWeek = date.getDay() // 0 es Domingo, 1 es Lunes, ...
+    const month = date.getMonth() + 1
+    const dayOfWeek = date.getDay()
   
-    return `${minute} ${hour} ${dayOfMonth} ${month} ${dayOfWeek} comando`;
+    return `${minute} ${hour} ${dayOfMonth} ${month} ${dayOfWeek}`
 }
