79194acc66faef8465a9041623b092f7262261d3
Jorge <admin@maaide.com>
2023-06-19 23:54:33 -0400

    actualization general

--- src/controllers/campaigns.controllers.js
+++ src/controllers/campaigns.controllers.js
@@ -2,6 +2,7 @@ import Email from '../models/Email.js'
 import cron from 'node-cron'
 import { sendEmail } from '../utils/sendEmail.js'
 import { formatDateToCron } from '../utils/cronFormat.js'
+import Client from '../models/Client.js'
 
 export const createCampaign = async (req, res) => {
     try {
@@ -9,8 +10,15 @@ export const createCampaign = async (req, res) => {
         const newCampaign = new Email({ address, affair, summary, title, paragraph, buttonText, url, date })
         await newCampaign.save()
         const format = formatDateToCron(date)
+        let subscribers = []
+        if (address === 'Todos los suscriptores') {
+            subscribers = await Client.find().lean()
+        }
         cron.schedule(format, () => {
-          sendEmail({ address, affair, title, paragraph, buttonText, url })
+            subscribers.map(subscriber => {
+                const mail = subscriber.email
+                sendEmail({ mail, affair, title, paragraph, buttonText, url })
+            })
         })
         return res.send(newCampaign)
     } catch (error) {
