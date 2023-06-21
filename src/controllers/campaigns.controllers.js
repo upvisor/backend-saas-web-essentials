d09e96af787aa3aa0b5ee86ab00307b84d427b43
Jorge <contacto@maaide.com>
2023-06-21 12:41:29 -0400

    actualization general

--- src/controllers/campaigns.controllers.js
+++ src/controllers/campaigns.controllers.js
@@ -7,20 +7,25 @@ import Client from '../models/Client.js'
 export const createCampaign = async (req, res) => {
     try {
         const { address, affair, summary, title, paragraph, buttonText, url, date } = req.body
-        const newCampaign = new Email({ address, affair, summary, title, paragraph, buttonText, url, date })
-        await newCampaign.save()
-        const dateFormat = new Date(date)
-        const format = formatDateToCron(dateFormat)
         let subscribers = []
         if (address === 'Todos los suscriptores') {
             subscribers = await Client.find().lean()
         }
-        console.log(subscribers)
-        cron.schedule(format, () => {
+        const newCampaign = new Email({ address: subscribers, affair, summary, title, paragraph, buttonText, url, date })
+        await newCampaign.save()
+        if (date === undefined) {
             subscribers.map(subscriber => {
                 sendEmail({ address: subscriber.email, affair, title, paragraph, buttonText, url })
             })
-        })
+        } else {
+            const dateFormat = new Date(date)
+            const format = formatDateToCron(dateFormat)
+            cron.schedule(format, () => {
+                subscribers.map(subscriber => {
+                    sendEmail({ address: subscriber.email, affair, title, paragraph, buttonText, url })
+                })
+            })
+        }
         return res.send(newCampaign)
     } catch (error) {
         return res.status(500).json({message: error.message})
