5160b0e7c4078d511cd064e8ee478dd65c9a44fa
jorge <contacto@maaide.com>
2023-06-22 10:59:25 -0400

    actualization general

--- src/controllers/campaigns.controllers.js
+++ src/controllers/campaigns.controllers.js
@@ -25,7 +25,7 @@ export const createCampaign = async (req, res) => {
             const format = formatDateToCron(dateFormat)
             cron.schedule(format, () => {
                 subscribers.map(subscriber => {
-                    sendEmail({ address: subscriber.email, name: subscriber.firstName, affair, title, paragraph, buttonText, url, storeData: storeData[0] }).catch(console.error)
+                    sendEmail({ address: subscriber.email, name: subscriber.firstName !== undefined ? subscriber.firstName : '', affair, title, paragraph, buttonText, url, storeData: storeData[0] }).catch(console.error)
                 })
             }, {
                 scheduled: true,
