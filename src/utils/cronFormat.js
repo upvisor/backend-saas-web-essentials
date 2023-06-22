97547d2bbb743d040977867f4dc58930f102e60e
jorge <contacto@maaide.com>
2023-06-22 10:36:45 -0400

    actualization general

--- src/utils/cronFormat.js
+++ src/utils/cronFormat.js
@@ -3,7 +3,6 @@ export function formatDateToCron(date) {
     const hour = date.getHours()
     const dayOfMonth = date.getDate()
     const month = date.getMonth() + 1
-    const dayOfWeek = date.getDay()
   
-    return `${minute} ${hour} ${dayOfMonth} ${month} ${dayOfWeek}`
+    return `${minute} ${hour} ${dayOfMonth} ${month} *`
 }
