5d1eb260a8c544d000e3c751ab8d58f5b4de54b4
jorge <contacto@maaide.com>
2023-06-30 15:46:58 -0400

    actualization general

--- src/controllers/stadistics.controllers.js
+++ src/controllers/stadistics.controllers.js
@@ -9,7 +9,7 @@ export const getStadistics = async (req, res) => {
     const addCarts = await AddCart.find().lean()
     const informations = await Information.find().lean()
     const sells = await Sell.find().lean()
-    return res.send([viewContents, addCarts, informations, sells])
+    return res.send([{ viewContents: viewContents }, { addCarts: addCarts }, { informations: informations }, { sells: sells }])
   } catch (error) {
     return res.status(500).json({message: error.message})
   }
@@ -18,22 +18,22 @@ export const getStadistics = async (req, res) => {
 export const getStadisticsFiltered = async (req, res) => {
   try {
     const {dateInitial, dateLast} = req.body
-    console.log(new Date(dateInitial))
-    console.log(new Date(dateLast))
+    const dateInitialFormat = new Date(dateInitial)
+    const dateLastFormat = new Date(dateLast)
     let stadistics = []
-    const viewContents = await ViewContent.find({ createdAt: { $gte: dateInitial, $lte: dateLast } }).sort({ createdAt: 1 })
+    const viewContents = await ViewContent.find({ createdAt: { $gte: dateInitialFormat, $lte: dateLastFormat } }).sort({ createdAt: 1 })
     if (viewContents) {
       stadistics = stadistics.concat({ viewContents: viewContents })
     }
-    const addCarts = await AddCart.find({ createdAt: { $gte: new Date(dateInitial), $lte: new Date(dateLast) } }).sort({ createdAt: 1 })
+    const addCarts = await AddCart.find({ createdAt: { $gte: dateInitialFormat, $lte: dateLastFormat } }).sort({ createdAt: 1 })
     if (addCarts) {
       stadistics = stadistics.concat({ addCarts: addCarts })
     }
-    const informations = await Information.find({ createdAt: { $gte: new Date(dateInitial), $lte: new Date(dateLast) } }).sort({ createdAt: 1 })
+    const informations = await Information.find({ createdAt: { $gte: dateInitialFormat, $lte: dateLastFormat } }).sort({ createdAt: 1 })
     if (informations) {
       stadistics = stadistics.concat({ informations: informations })
     }
-    const sells = await Sell.find({ createdAt: { $gte: new Date(dateInitial), $lte: new Date(dateLast) } }).sort({ createdAt: 1 })
+    const sells = await Sell.find({ createdAt: { $gte: dateInitialFormat, $lte: dateLastFormat } }).sort({ createdAt: 1 })
     if (sells) {
       stadistics = stadistics.concat({ sells: sells })
     }
