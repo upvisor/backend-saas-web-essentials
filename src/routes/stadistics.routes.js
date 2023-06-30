8f801372d336c4e8d6617bb941e39dae37f3cd4e
jorge <contacto@maaide.com>
2023-06-30 14:50:37 -0400

    actualization general

--- src/routes/stadistics.routes.js
+++ src/routes/stadistics.routes.js
@@ -1,8 +1,10 @@
 import {Router} from 'express'
-import {getStadistics} from '../controllers/stadistics.controllers.js'
+import {getStadistics, getStadisticsFiltered} from '../controllers/stadistics.controllers.js'
 
 const router = Router()
 
 router.get('/stadistics', getStadistics)
+
+router.post('/stadistics', getStadisticsFiltered)
 
 export default router
