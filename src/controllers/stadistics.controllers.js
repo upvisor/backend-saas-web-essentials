565111010fbd3dd3121b09f3f6651ab8cdc1f3b7
jorge <contacto@maaide.com>
2023-06-30 15:32:51 -0400

    actualization general

--- src/controllers/stadistics.controllers.js
+++ src/controllers/stadistics.controllers.js
@@ -18,6 +18,7 @@ export const getStadistics = async (req, res) => {
 export const getStadisticsFiltered = async (req, res) => {
   try {
     const {dateInitial, dateLast} = req.body
+    console.log(req.body)
     const filters = {}
     if (dateInitial && dateLast) {
       filters.createdAt = {
@@ -28,19 +29,19 @@ export const getStadisticsFiltered = async (req, res) => {
     let stadistics = []
     const viewContents = await ViewContent.find(filters).sort({ createdAt: 1 })
     if (viewContents) {
-      stadistics = stadistics.concat({viewContents: viewContents})
+      stadistics = stadistics.concat([viewContents])
     }
     const addCarts = await AddCart.find(filters).sort({ createdAt: 1 })
     if (addCarts) {
-      stadistics = stadistics.concat({addCarts: addCarts})
+      stadistics = stadistics.concat([addCarts])
     }
     const informations = await Information.find(filters).sort({ createdAt: 1 })
     if (informations) {
-      stadistics = stadistics.concat({informations: informations})
+      stadistics = stadistics.concat([informations])
     }
     const sells = await Sell.find(filters).sort({ createdAt: 1 })
     if (sells) {
-      stadistics = stadistics.concat({sells: sells})
+      stadistics = stadistics.concat([sells])
     }
     return res.send(stadistics)
   } catch (error) {
