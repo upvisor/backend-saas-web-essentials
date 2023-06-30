41f396d11a89d71ca7bcf2786d51bc06179df8b0
jorge <contacto@maaide.com>
2023-06-30 11:58:55 -0400

    actualization general

--- src/controllers/storeData.controllers.js
+++ src/controllers/storeData.controllers.js
@@ -2,18 +2,15 @@ import StoreData from '../models/StoreData.js'
 
 export const createStoreData = async (req, res) => {
     try {
-        const storeData = new StoreData(req.body)
-        await storeData.save()
-        return res.json(storeData)
-    } catch (error) {
-        return res.status(500).json({message: error.message})
-    }
-}
-
-export const editStoreData = async (req, res) => {
-    try {
-        const storeData = await StoreData.findByIdAndUpdate(req.params.id, req.body, {new: true})
-        return res.json(storeData)
+        const data = await StoreData.findOne().lean()
+        if (data) {
+            const editStoreData = await StoreData.findOneAndUpdate(req.body, { new: true })
+            return res.json(editStoreData)
+        } else {
+            const storeData = new StoreData(req.body)
+            await storeData.save()
+            return res.json(storeData)
+        }
     } catch (error) {
         return res.status(500).json({message: error.message})
     }
--- src/routes/storeData.routes.js
+++ src/routes/storeData.routes.js
@@ -1,11 +1,9 @@
 import {Router} from 'express'
-import { createStoreData, editStoreData, getStoreData } from '../controllers/storeData.controllers.js'
+import { createStoreData, getStoreData } from '../controllers/storeData.controllers.js'
 
 const router = Router()
 
 router.post('/store-data', createStoreData)
-
-router.put('/store-data/:id', editStoreData)
 
 router.get('/store-data', getStoreData)
 
