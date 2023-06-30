d0c5cbc408ee321ed285f4d83350b8f0bc0c3ad5
jorge <contacto@maaide.com>
2023-06-30 12:04:44 -0400

    actualization general

--- src/controllers/storeData.controllers.js
+++ src/controllers/storeData.controllers.js
@@ -4,13 +4,11 @@ export const createStoreData = async (req, res) => {
     try {
         const data = await StoreData.findOne().lean()
         if (data) {
-            const editStoreData = await StoreData.findOneAndUpdate(req.body, { new: true })
-            return res.json(editStoreData)
-        } else {
-            const storeData = new StoreData(req.body)
-            await storeData.save()
-            return res.json(storeData)
+            await StoreData.findOneAndDelete(data._id)
         }
+        const storeData = new StoreData(req.body)
+        await storeData.save()
+        return res.json(storeData)
     } catch (error) {
         return res.status(500).json({message: error.message})
     }
