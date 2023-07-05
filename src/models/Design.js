e04d3c38ab2971d02152f3bd1355c79241034ebf
jorge <contacto@maaide.com>
2023-07-05 13:06:53 -0400

    actualization general

--- src/models/Design.js
+++ src/models/Design.js
@@ -1,54 +1,42 @@
 import mongoose from 'mongoose'
 
-const DesignSchema = new mongoose.Schema([
-    {
-        header: {
-            topStrip: { type: String }
-        }
+const DesignSchema = new mongoose.Schema({
+    header: {
+        topStrip: { type: String }
     },
-    {
-        home: {
-            banner: [{
-                image: { type: String },
-                title: { type: String },
-                text: { type: String },
-                textButton: { type: String },
-                linkButton: { type: String }
-            }],
-            category: {
-                titleCategory: { type: Boolean },
-                descriptionCategory: { type: Boolean }
-            },
-            products: {
-                title: { type: String },
-                sectionProducts: { type: String }
-            }
-        }
-    },
-    {
-        productPage: {
-
-        }
-    },
-    {
-        contactPage: {
+    home: {
+        banner: [{
+            image: { type: String },
             title: { type: String },
             text: { type: String },
-            titleForm: { type: String }
-        }
-    },
-    {
-        shopPage: {
+            textButton: { type: String },
+            linkButton: { type: String }
+        }],
+        category: {
+            titleCategory: { type: Boolean },
+            descriptionCategory: { type: Boolean }
+        },
+        products: {
             title: { type: String },
-            description: { type: String }
+            sectionProducts: { type: String }
         }
     },
-    {
-        subscriptionZone: {
-            title: { type: String }
-        }
+    product: {
+        sectionProducts: { type: String }
+    },
+    contact: {
+        title: { type: String },
+        text: { type: String },
+        titleForm: { type: String }
+    },
+    shop: {
+        title: { type: String },
+        description: { type: String }
+    },
+    subscription: {
+        title: { type: String }
     }
-], {
+}, {
     timestamps: true
 })
 
