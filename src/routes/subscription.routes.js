1b0841ba912c1df6afb814998012715489a2c8d9
jorge <contacto@maaide.com>
2023-07-10 19:05:16 -0400

    actualization general

--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -3,6 +3,6 @@ import { createSubscription } from '../controllers/subscription.controllers.js'
 
 const router = Router()
 
-router.post('subscription', createSubscription)
+router.post('/subscription', createSubscription)
 
 export default router
