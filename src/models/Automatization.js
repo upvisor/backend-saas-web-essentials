f7309bf3f6cedc201c4a92eb15c3a2793f1e1318
jorge <contacto@maaide.com>
2023-07-18 12:44:12 -0400

    actualization general

--- src/models/Automatization.js
+++ src/models/Automatization.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const AutomatizationSchema = new mongoose.Schema({
     address: { type: String, required: true },
     name: { type: String, required: true },
-    automatization: [{ affair: { type: String, required: true }, title: { type: String, required: true }, paragraph: { type: String, required: true }, buttonText: { type: String }, url: { type: String }, date: { type: Date } }]
+    automatization: [{ affair: { type: String, required: true }, title: { type: String, required: true }, paragraph: { type: String, required: true }, buttonText: { type: String }, url: { type: String }, date: { type: Date }, number: { type: Number }, time: { type: String } }]
 }, {
     timestamps: true
 })
