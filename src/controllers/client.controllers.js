f9d8481f4bac2d07faa62de68bcb49288468cfd7
jorge <contacto@maaide.com>
2023-07-19 11:00:42 -0400

    actualization general

--- src/controllers/client.controllers.js
+++ src/controllers/client.controllers.js
@@ -9,72 +9,76 @@ export const createClient = async (req, res) => {
   try {
     const client = await Client.findOne({email: req.body.email}).lean()
     if (client) {
-      const clientTagsSet = new Set(client.tags)
-      const reqBodyTagsSet = new Set(req.body.tags)
-      reqBodyTagsSet.forEach(tag => clientTagsSet.add(tag))
-      const updatedTags = Array.from(clientTagsSet)
-      client.tags = updatedTags
-      const editClien = await Client.findByIdAndUpdate(client._id, client, { new: true })
-      const automatizations = await Automatization.find().lean()
-      const automatizationsClient = automatizations.filter(automatization => req.body.tags.includes(automatization.address) || automatization.address === 'Todos los suscriptores')
-      let emails = []
-      automatizationsClient.map(async (automatization) => {
-          let previousDate = new Date()
-          previousDate.setMinutes(previousDate.getMinutes() + 1)
-          for (const email of automatization.automatization) {
-              const currentDate = new Date(previousDate)
-              if (email.time === 'Días') {
-                  currentDate.setDate(currentDate.getDate() + Number(email.number))
-              } else if (email.time === 'Horas') {
-                  currentDate.setHours(currentDate.getHours() + Number(email.number))
-              } else if (email.time === 'Minutos') {
-                  currentDate.setMinutes(currentDate.getMinutes() + Number(email.number))
-              }
-              email.date = currentDate
-              emails.push(email)
-              previousDate = currentDate
-          }
-          emails.map(async (email) => {
-              const storeData = await StoreData.find().lean()
-              const dateFormat = new Date(email.date)
-              const format = formatDateToCron(dateFormat)
-              cron.schedule(format, () => {
-                  sendEmailAutomatization({ address: client.email, name: client.firstName !== undefined ? client.firstName : '', affair: email.affair, title: email.title, paragraph: email.paragraph, buttonText: email.buttonText, url: email.url, storeData: storeData[0] }).catch(console.error)
-              })
-          })
-      })
+      if (req.body.tags) {
+        const clientTagsSet = new Set(client.tags)
+        const reqBodyTagsSet = new Set(req.body.tags)
+        reqBodyTagsSet.forEach(tag => clientTagsSet.add(tag))
+        const updatedTags = Array.from(clientTagsSet)
+        client.tags = updatedTags
+        const editClien = await Client.findByIdAndUpdate(client._id, client, { new: true })
+        const automatizations = await Automatization.find().lean()
+        const automatizationsClient = automatizations.filter(automatization => req.body.tags.includes(automatization.address) || automatization.address === 'Todos los suscriptores')
+        let emails = []
+        automatizationsClient.map(async (automatization) => {
+            let previousDate = new Date()
+            previousDate.setMinutes(previousDate.getMinutes() + 1)
+            for (const email of automatization.automatization) {
+                const currentDate = new Date(previousDate)
+                if (email.time === 'Días') {
+                    currentDate.setDate(currentDate.getDate() + Number(email.number))
+                } else if (email.time === 'Horas') {
+                    currentDate.setHours(currentDate.getHours() + Number(email.number))
+                } else if (email.time === 'Minutos') {
+                    currentDate.setMinutes(currentDate.getMinutes() + Number(email.number))
+                }
+                email.date = currentDate
+                emails.push(email)
+                previousDate = currentDate
+            }
+            emails.map(async (email) => {
+                const storeData = await StoreData.find().lean()
+                const dateFormat = new Date(email.date)
+                const format = formatDateToCron(dateFormat)
+                cron.schedule(format, () => {
+                    sendEmailAutomatization({ address: client.email, name: client.firstName !== undefined ? client.firstName : '', affair: email.affair, title: email.title, paragraph: email.paragraph, buttonText: email.buttonText, url: email.url, storeData: storeData[0] }).catch(console.error)
+                })
+            })
+        })
+      }
       return res.send(editClien)
     } else {
       const newClient = new Client(req.body)
       await newClient.save()
-      const automatizations = await Automatization.find().lean()
-      const automatizationsClient = automatizations.filter(automatization => req.body.tags.includes(automatization.address) || automatization.address === 'Todos los suscriptores')
-      let emails = []
-      automatizationsClient.map(async (automatization) => {
-          let previousDate = new Date()
-          previousDate.setMinutes(previousDate.getMinutes() + 1)
-          for (const email of automatization.automatization) {
-              const currentDate = new Date(previousDate)
-              if (email.time === 'Días') {
-                  currentDate.setDate(currentDate.getDate() + Number(email.number))
-              } else if (email.time === 'Horas') {
-                  currentDate.setHours(currentDate.getHours() + Number(email.number))
-              } else if (email.time === 'Minutos') {
-                  currentDate.setMinutes(currentDate.getMinutes() + Number(email.number))
-              }
-              email.date = currentDate
-              emails.push(email)
-              previousDate = currentDate
-          }
-          emails.map(async (email) => {
-              const storeData = await StoreData.findOne().lean()
-              const dateFormat = new Date(email.date)
-              const format = formatDateToCron(dateFormat)
-              cron.schedule(format, () => {
-                  sendEmailAutomatization({ address: req.body.email, name: req.body.firstName !== undefined ? req.body.firstName : '', affair: email.affair, title: email.title, paragraph: email.paragraph, buttonText: email.buttonText, url: email.url, storeData: storeData === null ? { name: '', email: '', phone: '', address: '', city: '', region: '' } : storeData }).catch(console.error)
-              })
-          })
-      })
+      if (req.body.tags) {
+        const automatizations = await Automatization.find().lean()
+        const automatizationsClient = automatizations.filter(automatization => req.body.tags.includes(automatization.address) || automatization.address === 'Todos los suscriptores')
+        let emails = []
+        automatizationsClient.map(async (automatization) => {
+            let previousDate = new Date()
+            previousDate.setMinutes(previousDate.getMinutes() + 1)
+            for (const email of automatization.automatization) {
+                const currentDate = new Date(previousDate)
+                if (email.time === 'Días') {
+                    currentDate.setDate(currentDate.getDate() + Number(email.number))
+                } else if (email.time === 'Horas') {
+                    currentDate.setHours(currentDate.getHours() + Number(email.number))
+                } else if (email.time === 'Minutos') {
+                    currentDate.setMinutes(currentDate.getMinutes() + Number(email.number))
+                }
+                email.date = currentDate
+                emails.push(email)
+                previousDate = currentDate
+            }
+            emails.map(async (email) => {
+                const storeData = await StoreData.findOne().lean()
+                const dateFormat = new Date(email.date)
+                const format = formatDateToCron(dateFormat)
+                cron.schedule(format, () => {
+                    sendEmailAutomatization({ address: req.body.email, name: req.body.firstName !== undefined ? req.body.firstName : '', affair: email.affair, title: email.title, paragraph: email.paragraph, buttonText: email.buttonText, url: email.url, storeData: storeData === null ? { name: '', email: '', phone: '', address: '', city: '', region: '' } : storeData }).catch(console.error)
+                })
+            })
+        })
+      }
       return res.json(newClient)
     }
   } catch (error) {
