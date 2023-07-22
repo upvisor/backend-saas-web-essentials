957f36149861b75858b39b0a6cbef390a6029f40
jorge <contacto@maaide.com>
2023-07-22 11:54:36 -0400

    actualization general

--- src/models/StoreData.js
+++ src/models/StoreData.js
@@ -3,11 +3,20 @@ import mongoose from 'mongoose'
 const StoreDataSchema = mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
-  phone: { type: Number, required: true },
-  address: { type: String, required: true },
+  phone: { type: Number },
+  address: { type: String },
   departament: { type: String },
-  region: { type: String, required: true },
-  city: { type: String, required: true },
+  region: { type: String },
+  city: { type: String },
+  schedule: {
+    monday: { state: { type: Boolean, required: true }, open: { type: String, required: true }, close: { type: String, required: true } },
+    thesday: { state: { type: Boolean, required: true }, open: { type: String, required: true }, close: { type: String, required: true } },
+    wednesday: { state: { type: Boolean, required: true }, open: { type: String, required: true }, close: { type: String, required: true } },
+    thursday: { state: { type: Boolean, required: true }, open: { type: String, required: true }, close: { type: String, required: true } },
+    friday: { state: { type: Boolean, required: true }, open: { type: String, required: true }, close: { type: String, required: true } },
+    saturday: { state: { type: Boolean, required: true }, open: { type: String, required: true }, close: { type: String, required: true } },
+    sunday: { state: { type: Boolean, required: true }, open: { type: String, required: true }, close: { type: String, required: true } },
+  },
   logo: { public_id: { type: String }, url: { type: String } },
   logoWhite: { public_id: { type: String }, url: { type: String } }
 }, {
