ccb9c95e9055324b191c061db699267a654c2931
jorge <contacto@maaide.com>
2023-07-31 10:25:57 -0400

    actualization general

--- src/models/Product.js
+++ src/models/Product.js
@@ -9,7 +9,7 @@ const productSchema = new mongoose.Schema({
   beforePrice: { type: Number },
   cost: { type: Number },
   timeOffer: { type: String },
-  variations: { nameVariation: { type: String, required: true }, nameSubVariation: { type: String }, variations: [{ variation: { type: String }, subVariation: { type: String }, stock: { type: Number }, sku: { type: String }, image: { public_id: { type: String }, url: { type: String } } }], subVariation: { nameSubVariation: { type: String }, subVariations: [{ subVariation: { type: String }, stock: { type: Number }, sku: { type: String }, image: { public_id: { type: String }, url: { type: String } } }] } },
+  variations: { nameVariation: { type: String, required: true }, nameSubVariation: { type: String }, variations: [{ variation: { type: String }, subVariation: { type: String }, stock: { type: Number }, sku: { type: String }, image: { public_id: { type: String }, url: { type: String } } }] },
   productsOffer: [{ productsSale: [], price: { type: Number, required: true } }],
   slug: { type: String, required: true, unique: true },
   tags: [{ type: String }],
