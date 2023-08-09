16712a8892416f5d0639909b7e676654e5dfd61e
jorge <contacto@maaide.com>
2023-08-08 23:40:03 -0400

    actualization general

--- src/controllers/design.controllers.js
+++ src/controllers/design.controllers.js
@@ -32,6 +32,9 @@ export const createDesign = async (req, res) => {
 export const getDesign = async (req, res) => {
     try {
         const design = await Design.findOne().lean()
+        if (design === null) {
+            return res.send([])
+        }
         return res.send(design)
     } catch (error) {
         return res.status(500).json({message: error.message})
