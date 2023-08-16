1a19a565b3fc25d09562ad31735393751c43f580
jorge <contacto@maaide.com>
2023-08-16 12:38:49 -0400

    actualization general

--- src/utils/sendEmailBuy.js
+++ src/utils/sendEmailBuy.js
@@ -26,7 +26,7 @@ export const sendEmailBuy = async ({ sell, storeData }) => {
                 <main style="display: flex;">
                     <div style="margin: auto; width: 100%; padding: 12px; max-width: 600px;">
                         <div style="display: flex;">
-                            <a style="margin: auto;" target="_blank" href="https://tienda-1.vercel.app/"><img style="width: 300px;" src="${storeData.logo.url}" /></a>
+                            <a style="margin: auto;" target="_blank" href="https://tienda-1.vercel.app/">${storeData?.logo.url ? `<img style="width: 300px;" src="${storeData.logo.url}" />` : '<p style="font-size: 36px;">TIENDA</p>'}</a>
                         </div>
                         <h1 style="font-weight: 500; text-align: center; margin-bottom: 0px;">Aqui tienes todos los detalles de tu compra</h1>
                         <p style="text-align: center; font-size: 16px;">Hola ${sell.firstName}, tu compra ha sido realizada con exito, aqui te dejamos todos los detalles.</p>
@@ -47,20 +47,20 @@ export const sendEmailBuy = async ({ sell, storeData }) => {
                         <div style="border-top: 1px solid #CACACA; padding: 12px;">
                             <div style="width: 100%; display: flex; margin-bottom: 10px;">
                                 <div style="width: auto; display: flex; margin: auto;">
-                                    <a style="padding-right: 30px;" href="${storeData.instagram}" target="_blank"><img style="width: 25px; height: 25px;" src="https://cdn.icon-icons.com/icons2/836/PNG/512/Instagram_icon-icons.com_66804.png" /></a>
-                                    <a style="padding-right: 30px;" href="${storeData.facebook}" target="_blank"><img style="width: 25px; height: 25px;" src="https://cdn.icon-icons.com/icons2/2429/PNG/512/facebook_logo_icon_147291.png" /></a>
-                                    <a style="padding-right: 30px;" href="${storeData.tiktok}" target="_blank"><img style="width: 25px; height: 25px;" src="https://cdn.icon-icons.com/icons2/2972/PNG/512/tiktok_logo_icon_186896.png" /></a>
-                                    <a href="${storeData.whatsapp}" target="_blank"><img style="width: 25px; height: 25px;" src="https://cdn.icon-icons.com/icons2/729/PNG/512/whatsapp_icon-icons.com_62756.png" /></a>
+                                    ${storeData?.instagram ? `<a style="padding-right: 30px;" href="${storeData.instagram}" target="_blank"><img style="width: 25px; height: 25px;" src="https://cdn.icon-icons.com/icons2/836/PNG/512/Instagram_icon-icons.com_66804.png" /></a>` : ''}
+                                    ${storeData?.facebook ? `<a style="padding-right: 30px;" href="${storeData.facebook}" target="_blank"><img style="width: 25px; height: 25px;" src="https://cdn.icon-icons.com/icons2/2429/PNG/512/facebook_logo_icon_147291.png" /></a>` : ''}
+                                    ${storeData?.tiktok ? `<a style="padding-right: 30px;" href="${storeData.tiktok}" target="_blank"><img style="width: 25px; height: 25px;" src="https://cdn.icon-icons.com/icons2/2972/PNG/512/tiktok_logo_icon_186896.png" /></a>` : ''}
+                                    ${storeData?.whatsapp ? `<a href="${storeData.whatsapp}" target="_blank"><img style="width: 25px; height: 25px;" src="https://cdn.icon-icons.com/icons2/729/PNG/512/whatsapp_icon-icons.com_62756.png" /></a>` :''}
                                 </div>
                             </div>
                             <div style="width: 100%; display: flex; margin-bottom: 4px;">
                                 <div style="width: auto; display: flex; margin: auto;">
-                                    <span style="font-size: 14px;">${storeData.email}</span>
+                                    <span style="font-size: 14px;">${storeData?.email ? storeData.email : ''}</span>
                                 </div>
                             </div>
                             <div style="width: 100%; display: flex; margin-bottom: 10px;">
                                 <div style="width: auto; display: flex; margin: auto;">
-                                    <span style="font-size: 14px;">${storeData.phone}</span>
+                                    <span style="font-size: 14px;">${storeData?.phone ? storeData.phone : ''}</span>
                                 </div>
                             </div>
                             <div style="width: 100%; display: flex;">
