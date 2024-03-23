ad271001b77a92b3e4049ff7c3705b8612015ba9
jorge <contacto@maaide.com>
2024-03-23 16:01:54 -0300

    actualization genreal

--- src/models/Product.js
+++ src/models/Product.js
@@ -9,7 +9,7 @@ const productSchema = new mongoose.Schema({
   beforePrice: { type: Number },
   cost: { type: Number },
   timeOffer: { type: String },
-  variations: { nameVariation: { type: String }, nameSubVariation: { type: String }, variations: [{ variation: { type: String }, subVariation: { type: String }, stock: { type: Number }, sku: { type: String }, image: { public_id: { type: String }, url: { type: String } } }] },
+  variations: { nameVariation: { type: String }, formatVariation: { type: String }, nameVariations: [{ variation: { type: String }, colorVariation: { type: String } }], nameSubVariation: { type: String }, formatSubVariation: { type: String }, nameSubVariations: [{ subVariation: { type: String }, colorSubVariation: { type: String } }], nameSubVariation2: { type: String }, formatSubVariation2: { type: String }, nameSubVariations2: [{ subVariation2: { type: String }, colorSubVariation2: { type: String } }], variations: [{ variation: { type: String }, subVariation: { type: String }, subVariation2: { type: String }, stock: { type: Number }, sku: { type: String }, image: { public_id: { type: String }, url: { type: String } } }] },
   productsOffer: [{ productsSale: [], price: { type: Number, required: true } }],
   slug: { type: String, required: true, unique: true },
   tags: [{ type: String }],
