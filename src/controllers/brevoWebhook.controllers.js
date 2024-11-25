ae30412a4a7aeb30fca4771ca4cf8365e328a31b
jorge <contacto@maaide.com>
2024-11-25 18:39:53 -0300

    actualization general

--- src/controllers/brevoWebhook.controllers.js
+++ src/controllers/brevoWebhook.controllers.js
@@ -1,13 +1,12 @@
 import { updateClientEmailStatusById } from '../utils/updateEmail.js'
 
 export const getStatus = async (req, res) => {
-    console.log(req.body)
     const event = req.body.event;
     const email = req.body.email
     const emailId = req.body.headers ? req.body.headers['X-Unique-Id'] : null
     
-    if (event === 'opened') {
-        await updateClientEmailStatusById(email, emailId, 'opened');
+    if (event === 'unique_opened') {
+        await updateClientEmailStatusById(email, emailId, 'unique_opened');
     } else if (event === 'clicked') {
         await updateClientEmailStatusById(email, emailId, 'clicked');
     }
--- src/utils/updateEmail.js
+++ src/utils/updateEmail.js
@@ -10,7 +10,7 @@ export const updateClientEmailStatus = async (email, emailData) => {
 export const updateClientEmailStatusById = async (email, emailId, updateData) => {
     const client = await Client.findOne({ email: email })
     let emailUpdate = client.emails.find(email => email.id === emailId)
-    if (updateData === 'opened') {
+    if (updateData === 'unique_opened') {
         emailUpdate.opened = true
     } else if (updateData === 'clicked') {
         emailUpdate.clicked = true
